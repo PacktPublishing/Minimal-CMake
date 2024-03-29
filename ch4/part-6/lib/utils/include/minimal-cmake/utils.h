00049e6ccf0acaaa320f3a6eae2b5eb694d6be09
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-03-29 17:48:52 +0000

    fix .c header only library (add static keyword)

--- ch4/part-6/lib/utils/include/minimal-cmake/utils.h
+++ ch4/part-6/lib/utils/include/minimal-cmake/utils.h
@@ -3,8 +3,16 @@
 
 #include <stdint.h>
 
-int32_t mc_try_wrap(const int32_t index, const int32_t dim) {
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+static int32_t mc_try_wrap(const int32_t index, const int32_t dim) {
   return ((index % dim) + dim) % dim;
 }
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
 #endif // MC_UTILS_H
