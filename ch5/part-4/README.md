88158388d3230605b6f61399aa30a493d6144607
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-03-31 13:21:13 +0100

    add part-4 (copied from part3)

---
+++ ch5/part-4/README.md
@@ -0,0 +1,3 @@
+# ch5/part-3
+
+Expanding our `CMakePresets.json` file.
---
+++ ch5/part-4/app/main.c
@@ -0,0 +1,103 @@
+// third-party dependencies
+#include <minimal-cmake-gol/gol.h>
+#include <timer.h>
+
+// system includes
+#include <memory.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_board(const mc_gol_board_t* board) {
+  const char alive_dead_display[] = {'*', '@'};
+  for (int32_t y = 0, height = mc_gol_board_height(board); y < height; y++) {
+    for (int32_t x = 0, width = mc_gol_board_width(board); x < width; x++) {
+      printf("%c", alive_dead_display[mc_gol_board_cell(board, x, y)]);
+    }
+    printf("\n");
+  }
+}
+
+int main(int argc, char** argv) {
+  (void)argc;
+  (void)argv;
+
+  timer_lib_initialize();
+
+  mc_gol_board_t* board = mc_gol_create_board(40, 27);
+
+  // gosper glider gun
+  mc_gol_set_board_cell(board, 2, 5, true);
+  mc_gol_set_board_cell(board, 2, 6, true);
+  mc_gol_set_board_cell(board, 3, 5, true);
+  mc_gol_set_board_cell(board, 3, 6, true);
+
+  mc_gol_set_board_cell(board, 12, 5, true);
+  mc_gol_set_board_cell(board, 12, 6, true);
+  mc_gol_set_board_cell(board, 12, 7, true);
+  mc_gol_set_board_cell(board, 13, 4, true);
+  mc_gol_set_board_cell(board, 13, 8, true);
+  mc_gol_set_board_cell(board, 14, 3, true);
+  mc_gol_set_board_cell(board, 14, 9, true);
+  mc_gol_set_board_cell(board, 15, 3, true);
+  mc_gol_set_board_cell(board, 15, 9, true);
+  mc_gol_set_board_cell(board, 16, 6, true);
+  mc_gol_set_board_cell(board, 17, 4, true);
+  mc_gol_set_board_cell(board, 17, 8, true);
+  mc_gol_set_board_cell(board, 18, 5, true);
+  mc_gol_set_board_cell(board, 18, 6, true);
+  mc_gol_set_board_cell(board, 18, 7, true);
+  mc_gol_set_board_cell(board, 19, 6, true);
+
+  mc_gol_set_board_cell(board, 22, 3, true);
+  mc_gol_set_board_cell(board, 22, 4, true);
+  mc_gol_set_board_cell(board, 22, 5, true);
+  mc_gol_set_board_cell(board, 23, 3, true);
+  mc_gol_set_board_cell(board, 23, 4, true);
+  mc_gol_set_board_cell(board, 23, 5, true);
+  mc_gol_set_board_cell(board, 24, 2, true);
+  mc_gol_set_board_cell(board, 24, 6, true);
+  mc_gol_set_board_cell(board, 26, 1, true);
+  mc_gol_set_board_cell(board, 26, 2, true);
+  mc_gol_set_board_cell(board, 26, 6, true);
+  mc_gol_set_board_cell(board, 26, 7, true);
+
+  mc_gol_set_board_cell(board, 36, 3, true);
+  mc_gol_set_board_cell(board, 36, 4, true);
+  mc_gol_set_board_cell(board, 37, 3, true);
+  mc_gol_set_board_cell(board, 37, 4, true);
+
+  // eater
+  mc_gol_set_board_cell(board, 27, 20, true);
+  mc_gol_set_board_cell(board, 27, 21, true);
+  mc_gol_set_board_cell(board, 28, 20, true);
+  mc_gol_set_board_cell(board, 28, 21, true);
+
+  mc_gol_set_board_cell(board, 32, 21, true);
+  mc_gol_set_board_cell(board, 31, 22, true);
+  mc_gol_set_board_cell(board, 33, 22, true);
+  mc_gol_set_board_cell(board, 32, 23, true);
+
+  mc_gol_set_board_cell(board, 34, 23, true);
+  mc_gol_set_board_cell(board, 34, 24, true);
+  mc_gol_set_board_cell(board, 34, 25, true);
+  mc_gol_set_board_cell(board, 35, 25, true);
+
+  for (;;) {
+    const double delay = 0.1; // wait for 1/10th of a second
+    const tick_t time = timer_current();
+    printf("\33[H\33[2J\33[3J");
+    print_board(board);
+    mc_gol_update_board(board);
+    while (timer_elapsed(time) < delay) {
+      // busy wait (ideally cross-platform sleep for some of this...)
+    }
+  }
+
+  mc_gol_destroy_board(board);
+
+  timer_lib_shutdown();
+
+  return 0;
+}
