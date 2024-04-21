7d2835de6276af0532f4cf22cc35166b0c75d97a
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-04-21 21:15:27 +0100

    update clone.sh

--- ch6/part-1/third-party/clone.sh
+++ ch6/part-1/third-party/clone.sh
@@ -2,5 +2,5 @@
 
 # check if we haven't already created the sdl directory
 if [ ! -d sdl ]; then
-  git clone https://github.com/libsdl-org/SDL.git --branch release-2.30.2 --single-branch sdl --depth 1
+  git clone https://github.com/libsdl-org/SDL.git --branch release-2.30.2 --depth 1 sdl
 fi
