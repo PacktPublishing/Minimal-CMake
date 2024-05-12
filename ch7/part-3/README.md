70b63e2371945367ead63c00f96e1db87f08b9d2
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-05-12 14:46:38 +0100

    add part-3

---
+++ ch7/part-3/README.md
@@ -0,0 +1,3 @@
+# ch7/part-3
+
+Show how to deal with nested dependencies when installing.
---
+++ ch7/part-3/app/compile-shader-windows.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM compile shaders
+
+if not exist shader\build mkdir shader\build
+
+REM simple shader
+..\third-party\install\bin\shaderc.exe ^
+-f shader\vs_vertcol.sc -o shader\build\vs_vertcol.bin ^
+--platform windows --type vertex --verbose -i ./ -p s_5_0
+
+..\third-party\install\bin\shaderc.exe ^
+-f shader\fs_vertcol.sc -o shader\build\fs_vertcol.bin ^
+--platform windows --type fragment --verbose -i ./ -p s_5_0
