d367135cda92cafb100f55fd080312e402e348ac
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-06-02 21:27:27 +0100

    add support for automatically compiling shaders using CMake

---
+++ ch8/part-3/app/cmake/compile-shader-macos.cmake
@@ -0,0 +1,34 @@
+option(USE_VERBOSE_SHADER_OUTPUT "" OFF)
+if(USE_VERBOSE_SHADER_OUTPUT)
+  set(VERBOSE_SHADER_OUTPUT --verbose)
+else()
+  set(QUIET_SHADER_OUTPUT ERROR_QUIET)
+endif()
+
+# cmake-format: off
+file(MAKE_DIRECTORY shader/build)
+execute_process(
+  COMMAND
+    third-party/install/bin/shaderc
+    -f shader/vs_vertcol.sc
+    -o shader/build/vs_vertcol.bin
+    --platform osx
+    --type vertex
+    -i ./
+    -p metal
+    ${VERBOSE_SHADER_OUTPUT}
+  ${QUIET_SHADER_OUTPUT}
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+execute_process(
+  COMMAND
+    third-party/install/bin/shaderc
+    -f shader/fs_vertcol.sc
+    -o shader/build/fs_vertcol.bin
+    --platform osx
+    --type fragment
+    -i ./
+    -p metal
+    ${VERBOSE_SHADER_OUTPUT}
+  ${QUIET_SHADER_OUTPUT}
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+# cmake-format: on
