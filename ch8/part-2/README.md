9450f389edd9a59e16845cb5ac7bb93293f3cc38
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-06-02 17:28:16 +0100

    minor formatting

--- ch8/part-2/README.md
+++ ch8/part-2/README.md
@@ -1,3 +1,3 @@
 # ch8/part-2
 
-Show an example of setting-up a super build.
+Show an example of setting-up a superbuild.
--- ch8/part-2/app/cmake/superbuild.cmake
+++ ch8/part-2/app/cmake/superbuild.cmake
@@ -3,8 +3,6 @@ ExternalProject_Add(
   DEPENDS SDL2 bgfx mc-gol mc-draw
   SOURCE_DIR ${PROJECT_SOURCE_DIR}
   BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}
-  CMAKE_ARGS
-    -DCMAKE_PREFIX_PATH=${CMAKE_CURRENT_SOURCE_DIR}/third-party/install
-    -DSUPERBUILD=OFF
-    ${build_type_arg}
+  CMAKE_ARGS -DCMAKE_PREFIX_PATH=${CMAKE_CURRENT_SOURCE_DIR}/third-party/install
+             -DSUPERBUILD=OFF ${build_type_arg}
   INSTALL_COMMAND "")
