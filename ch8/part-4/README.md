64540fc31b7ab2196195e4469dfe36b228831084
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-06-04 21:19:52 +0100

    add part-4

---
+++ ch8/part-4/README.md
@@ -0,0 +1,3 @@
+# ch8/part-3
+
+Show an example of building shaders using `add_custom_command`.
---
+++ ch8/part-4/app/cmake/superbuild.cmake
@@ -0,0 +1,8 @@
+ExternalProject_Add(
+  ${CMAKE_PROJECT_NAME}_superbuild
+  DEPENDS SDL2 bgfx mc-gol mc-draw
+  SOURCE_DIR ${PROJECT_SOURCE_DIR}
+  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}
+  CMAKE_ARGS -DCMAKE_PREFIX_PATH=${CMAKE_CURRENT_SOURCE_DIR}/third-party/install
+             -DSUPERBUILD=OFF ${build_type_arg}
+  INSTALL_COMMAND "")
