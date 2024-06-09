d44a96a0c9a1501da057108d7e09bc1b6c14e7a8
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-06-09 16:48:40 +0100

    update superbuild format

--- ch8/part-2/app/cmake/superbuild.cmake
+++
@@ -1,8 +0,0 @@
-ExternalProject_Add(
-  ${CMAKE_PROJECT_NAME}_superbuild
-  DEPENDS SDL2 bgfx mc-gol mc-draw
-  SOURCE_DIR ${PROJECT_SOURCE_DIR}
-  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}
-  CMAKE_ARGS -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} -DSUPERBUILD=OFF
-             ${build_type_arg}
-  INSTALL_COMMAND "")
