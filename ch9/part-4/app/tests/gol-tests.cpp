1ffd6fbcc5a6e7a57287860efe85fef04b4ef2ad
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-06-30 18:30:40 +0100

    add more consistent spacing between tests

--- ch9/part-4/app/tests/gol-tests.cpp
+++ ch9/part-4/app/tests/gol-tests.cpp
@@ -3,30 +3,33 @@
 #include <minimal-cmake-gol/gol.h>
 
 void RegisterGolTests(ImGuiTestEngine* e, mc_gol_board_t* board) {
-  ImGuiTest* t = NULL;
+  ImGuiTest* t = nullptr;
   t = IM_REGISTER_TEST(e, "gol-tests", "Pause simulation");
   t->TestFunc = [](ImGuiTestContext* ctx) {
     ctx->SetRef("Game of Life");
     ctx->ItemClick("Pause");
   };
+
   t = IM_REGISTER_TEST(e, "gol-tests", "Play simulation");
   t->TestFunc = [](ImGuiTestContext* ctx) {
     ctx->SetRef("Game of Life");
     ctx->ItemClick("Play");
   };
+
   t = IM_REGISTER_TEST(e, "gol-tests", "Restart simulation");
   t->TestFunc = [](ImGuiTestContext* ctx) {
     ctx->SetRef("Game of Life");
     ctx->ItemClick("Pause");
     ctx->ItemClick("Restart");
   };
+
   t = IM_REGISTER_TEST(e, "gol-tests", "Clear simulation");
   t->TestFunc = [](ImGuiTestContext* ctx) {
     ctx->SetRef("Game of Life");
     ctx->ItemClick("Clear");
   };
-  t = IM_REGISTER_TEST(e, "gol-tests", "Click board");
 
+  t = IM_REGISTER_TEST(e, "gol-tests", "Click board");
   t->UserData = board;
   t->TestFunc = [](ImGuiTestContext* ctx) {
     const mc_gol_board_t* board = (mc_gol_board_t*)ctx->Test->UserData;
