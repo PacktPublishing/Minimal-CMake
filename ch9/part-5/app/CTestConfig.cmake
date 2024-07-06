3ef7a5c2a3b0030a6a9abd72fce07ee5781c2b39
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-07-06 21:28:33 +0100

    update CTestConfig.cmake

--- ch9/part-5/app/CTestConfig.cmake
+++ ch9/part-5/app/CTestConfig.cmake
@@ -1,13 +1,6 @@
+cmake_minimum_required(VERSION 3.28)
+
 set(CTEST_PROJECT_NAME minimal-cmake)
 set(CTEST_NIGHTLY_START_TIME 01:00:00 UTC)
 set(SITE "minimal-cmake")
-
-if(CMAKE_VERSION VERSION_GREATER 3.14)
-  set(CTEST_SUBMIT_URL https://my.cdash.org/submit.php?project=minimal-cmake)
-else()
-  set(CTEST_DROP_METHOD "https")
-  set(CTEST_DROP_SITE "my.cdash.org")
-  set(CTEST_DROP_LOCATION "/submit.php?project=minimal-cmake")
-endif()
-
-set(CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_SUBMIT_URL https://my.cdash.org/submit.php?project=minimal-cmake)
