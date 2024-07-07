63d3875ae48cfdc00fc71890ae2e062f9a266044
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-07-07 22:15:21 +0100

    simplify CTestConfig.cmake file

--- ch9/part-5/app/CTestConfig.cmake
+++ ch9/part-5/app/CTestConfig.cmake
@@ -1,6 +1,2 @@
-cmake_minimum_required(VERSION 3.28)
-
 set(CTEST_PROJECT_NAME minimal-cmake)
-set(CTEST_NIGHTLY_START_TIME 01:00:00 UTC)
-set(SITE "minimal-cmake")
 set(CTEST_SUBMIT_URL https://my.cdash.org/submit.php?project=minimal-cmake)
