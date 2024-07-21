307b16d2838b5c81c503a085ed1bc80a708e08aa
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-07-21 21:58:07 +0100

    workaround for recompaction ninja failure

--- ch10/part-2/app/configure-build-package-windows.sh
+++ ch10/part-2/app/configure-build-package-windows.sh
@@ -2,4 +2,6 @@
 
 cmake --preset multi-ninja-super
 cmake --build build/multi-ninja-super --config Release
-cpack --config build/multi-ninja-super/CPackConfig.cmake -G NSIS64 -B package
+cmake --preset multi-ninja # workaround 'failed recompaction' error
+cmake --build build/multi-ninja --config Release
+cpack --config build/multi-ninja/CPackConfig.cmake -G NSIS64 -B package
