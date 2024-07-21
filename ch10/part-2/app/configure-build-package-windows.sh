10796a93ff444f2d877a31fbb1febb0ccb519e16
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-07-21 21:47:38 +0100

    add .sh/.bat scripts for build and package

---
+++ ch10/part-2/app/configure-build-package-windows.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cmake --preset multi-ninja-super
+cmake --build build/multi-ninja-super --config Release
+cpack --config build/multi-ninja-super/CPackConfig.cmake -G NSIS64 -B package
