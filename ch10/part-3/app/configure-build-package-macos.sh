fbad9f4216e67ea5433f494c62096d6cc13723c5
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-07-22 21:07:20 +0100

    provide example of building macOS bundle

--- ch10/part-3/app/configure-build-package-macos.sh
+++ ch10/part-3/app/configure-build-package-macos.sh
@@ -2,4 +2,4 @@
 
 cmake --preset multi-ninja-super
 cmake --build build/multi-ninja-super --config Release
-cpack --config build/multi-ninja-super/CPackConfig.cmake -G Bundle -B package
+cpack --config build/multi-ninja-super/CPackConfig.cmake -G DragNDrop -B package
