bbd139dbf0c8d3fe63d8bc972b2c6ea4370d2435
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-09-07 16:20:43 +0100

    ensure tests work after refactor to new folder

--- ch11/part-4/app/tests/shaders-compiled.cmake
+++ ch11/part-4/app/tests/shaders-compiled.cmake
@@ -1,9 +1,9 @@
 # verify shaders have been compiled
 
-if(NOT EXISTS ${CMAKE_SOURCE_DIR}/shader/build/vs_vertcol.bin)
+if(NOT EXISTS shader/build/vs_vertcol.bin)
   message(FATAL_ERROR "vs_vertcol.bin missing")
 endif()
 
-if(NOT EXISTS ${CMAKE_SOURCE_DIR}/shader/build/fs_vertcol.bin)
+if(NOT EXISTS shader/build/fs_vertcol.bin)
   message(FATAL_ERROR "fs_vertcol.bin missing")
 endif()
