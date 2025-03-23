354881303b5840ef9335cf86b1c0fbc64d29f32c
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-03-23 21:39:41 +0000

    wip fix for findmc-gol.cmake example

--- ch7/part-7/cmake/Findmc-gol.cmake
+++ ch7/part-7/cmake/Findmc-gol.cmake
@@ -9,15 +9,22 @@ find_library(
   NAMES game-of-life game-of-lifed
   PATHS ${mc-gol_PATH}/lib)
 
+# todo - windows only
+find_file(
+  mc-gol_DLL
+  NAMES game-of-life.dll game-of-lifed.dll
+  PATHS ${mc-gol_PATH}/bin)
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(mc-gol DEFAULT_MSG mc-gol_LIBRARY
+find_package_handle_standard_args(mc-gol DEFAULT_MSG mc-gol_LIBRARY mc-gol_DLL
                                   mc-gol_INCLUDE_DIR)
 
 # if the library has been found, define the imported target
 if(mc-gol_FOUND AND NOT TARGET minimal-cmake::game-of-life)
-  add_library(minimal-cmake::game-of-life UNKNOWN IMPORTED)
+  add_library(minimal-cmake::game-of-life SHARED IMPORTED)
   set_target_properties(
     minimal-cmake::game-of-life
-    PROPERTIES IMPORTED_LOCATION "${mc-gol_LIBRARY}"
+    PROPERTIES IMPORTED_IMPLIB "${mc-gol_LIBRARY}" 
+               IMPORTED_LOCATION "${mc-gol_DLL}"
                INTERFACE_INCLUDE_DIRECTORIES "${mc-gol_INCLUDE_DIR}")
 endif()
