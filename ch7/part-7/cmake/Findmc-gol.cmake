0fc9d14608ed66c825fc84c4753d29d586455e2b
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-03-30 22:44:56 +0100

    Fix for Findmc-gol.cmake (#4)
    * wip fix for findmc-gol.cmake example
    
    * make changes to Findmc-gol.cmake cross platform compatible

--- ch7/part-7/cmake/Findmc-gol.cmake
+++ ch7/part-7/cmake/Findmc-gol.cmake
@@ -9,15 +9,56 @@ find_library(
   NAMES game-of-life game-of-lifed
   PATHS ${mc-gol_PATH}/lib)
 
+# on Windows, attempt to locate a .dll file (if the library was built as shared)
+if(WIN32)
+  find_file(
+    mc-gol_DLL
+    NAMES game-of-life.dll game-of-lifed.dll
+    PATHS ${mc-gol_PATH}/bin)
+endif()
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(mc-gol DEFAULT_MSG mc-gol_LIBRARY
-                                  mc-gol_INCLUDE_DIR)
+
+# handle find_package standard args (include mc-gol_DLL in the shared library
+# case on Windows)
+if(mc-gol_DLL)
+  find_package_handle_standard_args(mc-gol DEFAULT_MSG mc-gol_LIBRARY
+                                    mc-gol_INCLUDE_DIR mc-gol_DLL)
+else()
+  find_package_handle_standard_args(mc-gol DEFAULT_MSG mc-gol_LIBRARY
+                                    mc-gol_INCLUDE_DIR)
+endif()
 
 # if the library has been found, define the imported target
 if(mc-gol_FOUND AND NOT TARGET minimal-cmake::game-of-life)
-  add_library(minimal-cmake::game-of-life UNKNOWN IMPORTED)
-  set_target_properties(
-    minimal-cmake::game-of-life
-    PROPERTIES IMPORTED_LOCATION "${mc-gol_LIBRARY}"
-               INTERFACE_INCLUDE_DIRECTORIES "${mc-gol_INCLUDE_DIR}")
+  if(WIN32)
+    # if a .dll file was found, reference .lib file using IMPORTED_LIB, and
+    # .dll file using IMPORTED_LOCATION, otherwise default to normal static
+    # behaviour
+    if(mc-gol_DLL)
+      add_library(minimal-cmake::game-of-life SHARED IMPORTED)
+      set_target_properties(
+        minimal-cmake::game-of-life
+        PROPERTIES IMPORTED_IMPLIB "${mc-gol_LIBRARY}"
+                   IMPORTED_LOCATION "${mc-gol_DLL}"
+                   INTERFACE_INCLUDE_DIRECTORIES "${mc-gol_INCLUDE_DIR}")
+    else()
+      add_library(minimal-cmake::game-of-life STATIC IMPORTED)
+      set_target_properties(
+        minimal-cmake::game-of-life
+        PROPERTIES IMPORTED_LOCATION "${mc-gol_LIBRARY}"
+                   INTERFACE_INCLUDE_DIRECTORIES "${mc-gol_INCLUDE_DIR}")
+    endif()
+  else()
+    cmake_path(GET mc-gol_LIBRARY EXTENSION LAST_ONLY mc-gol_LIBRARY_EXT)
+    if(${mc-gol_LIBRARY_EXT} STREQUAL ${CMAKE_SHARED_LIBRARY_SUFFIX})
+      add_library(minimal-cmake::game-of-life SHARED IMPORTED)
+    else()
+      add_library(minimal-cmake::game-of-life STATIC IMPORTED)
+    endif()
+    set_target_properties(
+      minimal-cmake::game-of-life
+      PROPERTIES IMPORTED_LOCATION "${mc-gol_LIBRARY}"
+                 INTERFACE_INCLUDE_DIRECTORIES "${mc-gol_INCLUDE_DIR}")
+  endif()
 endif()
